1ba61d8777985957240a7ecb192f55da4eef1070
Edward <edwardhoang21@gmail.com>
2020-09-18 20:28:07 +0000

    changed the port to make heroku work

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(req, res){
-	console.log("The YelpCamp server has started!");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
